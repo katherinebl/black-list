72903255be3ea9a533627643134da34acc7ca627
Katherine Briceño <kath.gbl@gmail.com>
2019-02-16 22:01:00 +0100

    Add filter by name

--- src/App.js
+++ src/App.js
@@ -8,8 +8,29 @@ class App extends Component {
     super(props);
 
     this.state = {
+      query: '',
       results: this.getSavedData()
     };
+    this.getQuery = this.getQuery.bind(this);
+  }
+
+  getQuery(e) {
+    const userQuery = e.currentTarget.value;
+    this.setState({
+      query : userQuery
+    })
+  }
+
+  filterThis() {
+    const filteredResults = this.state.results.filter(item => {
+      const fullName = `${item.name.first} ${item.name.last}`;
+      if (fullName.toUpperCase().includes(this.state.query.toUpperCase())) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+    return filteredResults;
   }
 
   saveData(newResults) {
@@ -41,23 +62,37 @@ class App extends Component {
   }
 
   render() {
+    const blackResults = this.filterThis();
     return (
       <div className="app">
-        <h1 className="app__title">The Black List of Employees</h1>
-        <ul className="app__list">
-        {this.state.results.map(item => {
-          return (
-            <li className="app__list-item" id={item.id} key={item.id}>
-              <div className="person">
-                <h2 className="person__name">{`${item.name.first} ${item.name.last}`}</h2>
-                <img className="person__image" src={item.picture.medium} alt={`${item.name.first} ${item.name.last}`}/>
-                <div className="person__age">{item.dob.age}</div>
-                <div className="person__city">{item.location.city}</div>
-              </div>
-            </li>
-          );
-        })}
-        </ul>
+
+        <header className="app__header">
+          <h1 className="app__title">The Black List of Employees</h1>
+          <div className="app__filter">
+            <div className="app__filter-item">
+              <input className="app__filter-full-name" type="text" placeholder="Search for the guilty..." onKeyUp={this.getQuery} />
+            </div>
+          </div>
+
+        </header>
+
+        <main className="app__main">
+          <ul className="app__list">
+          {blackResults.map(item => {
+            return (
+              <li className="app__list-item" id={item.id} key={item.id}>
+                <div className="person">
+                  <h2 className="person__name">{`${item.name.first} ${item.name.last}`}</h2>
+                  <img className="person__image" src={item.picture.medium} alt={`${item.name.first} ${item.name.last}`}/>
+                  <div className="person__age">{item.dob.age}</div>
+                  <div className="person__city">{item.location.city}</div>
+                </div>
+              </li>
+            );
+          })}
+          </ul>
+
+        </main>
       </div>
     );
   }
