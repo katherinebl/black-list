6b00d7744ad7c7779627175dc53b80a8cc779e91
Katherine Briceño <kath.gbl@gmail.com>
2019-02-18 23:21:18 +0100

    Add conditional to message when there is no available data

--- src/components/BlackCard.js
+++ src/components/BlackCard.js
@@ -4,20 +4,25 @@ import PropTypes from 'prop-types';
 class BlackCard extends Component {
   render() {
     const {blackResults, blackId} = this.props;
-    const selectedPerson = blackResults[blackId];
-    const fullName = `${selectedPerson.name.first} ${selectedPerson.name.last}`;
-    const image = selectedPerson.picture.large;
-    const age = selectedPerson.dob.age;
-    const city = selectedPerson.location.city;
 
-    return (
-        <div className="person person--detail">
-            <h2 className="person__name">{fullName}</h2>
-            <img className="person__image" src={image} alt={fullName} />
-            <div className="person__age">{age}</div>
-            <div className="person__city">{city}</div>
-        </div>
-    );
+    if (blackResults.length === 0 || blackId >= blackResults.length) {
+      return <p>There is no available data</p>
+    } else {
+      const selectedPerson = blackResults[blackId];
+      const fullName = `${selectedPerson.name.first} ${selectedPerson.name.last}`;
+      const image = selectedPerson.picture.large;
+      const age = selectedPerson.dob.age;
+      const city = selectedPerson.location.city;
+  
+      return (
+          <div className="person person--detail">
+              <h2 className="person__name">{fullName}</h2>
+              <img className="person__image" src={image} alt={fullName} />
+              <div className="person__age">{age}</div>
+              <div className="person__city">{city}</div>
+          </div>
+      );
+    }
   }
 }
 
