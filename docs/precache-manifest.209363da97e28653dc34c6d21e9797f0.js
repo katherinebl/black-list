7306181e93584fda5a9fc6e2c1353f2235c712c0
Katherine Briceño <kath.gbl@gmail.com>
2019-02-24 21:58:21 +0100

    2nd run build

---
+++ docs/precache-manifest.209363da97e28653dc34c6d21e9797f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "8929b45b835b935707c5",
+    "url": "./static/js/main.8929b45b.chunk.js"
+  },
+  {
+    "revision": "e3a2d42c1502c8c269d3",
+    "url": "./static/js/2.e3a2d42c.chunk.js"
+  },
+  {
+    "revision": "8929b45b835b935707c5",
+    "url": "./static/css/main.3077b3f3.chunk.css"
+  },
+  {
+    "revision": "0be56db8d7ba4281f37bbac61fa48507",
+    "url": "./index.html"
+  }
+];
