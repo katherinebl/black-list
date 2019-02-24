e35ea04106933be3c7b3bbce1980de3b8905cefb
Katherine Briceño <kath.gbl@gmail.com>
2019-02-24 16:25:52 +0100

    Add build docs

---
+++ docs/precache-manifest.56940810d81ba13d8e5172cecc60015d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "62c21561162e03f6e319",
+    "url": "/static/js/main.62c21561.chunk.js"
+  },
+  {
+    "revision": "e3a2d42c1502c8c269d3",
+    "url": "/static/js/2.e3a2d42c.chunk.js"
+  },
+  {
+    "revision": "62c21561162e03f6e319",
+    "url": "/static/css/main.2561f4eb.chunk.css"
+  },
+  {
+    "revision": "51cd3d999e9ba081a4aebf629feb3937",
+    "url": "/index.html"
+  }
+];
